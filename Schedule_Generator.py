642206f135eae11768de23be47f45c746e41737f
MacErnest :^) <macernest@MacErnests-Air.lan>
2018-01-28 15:41:11 +0800

    added a few more things and made documentation for the basic Schedule Generator.

--- Schedule_Generator.py
+++ Schedule_Generator.py
@@ -10,19 +10,24 @@ class schedule:
                          [None, None, None, None, None],
                          [None, None, None, None, None],
                          [None, None, None, None, None]]
+        # creates a 2d array manually full of nothing because using the
+        # multiply operator of python does weird things to it
         self.free = []
         for x in range(5):
             for y in range(9):
                 self.free.append(str(x) + str(y))
+        # creates an array to list out all the free indices in schedule array
 
     def gen_schedule(self, list_sub):
         # generates a randomized non-conflicting schedule
         from random import randrange
         for sub in list_sub:
             _randomindex = str(self.free.pop(randrange(0, len(self.free))))
+            # gets a random index from the free array by popping it out
             self.schedule[int(_randomindex[1])][int(_randomindex[0])] = sub
 
     def output_schedule(self):
+        # outputs the schedule by printing it out
         s = ''
         for i in range(9):
             for j in range(5):
