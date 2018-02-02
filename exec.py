d061e9f6b67e7ba3703ad1dfc7f26771bfcf7dcb
MacErnest :^) <andreimartillo2000@gmail.com>
2018-02-02 13:08:34 +0800

    did some stuff

--- exec.py
+++ exec.py
@@ -1,6 +1,6 @@
 from Schedule_Generator import schedule
 
 sched = schedule()
-liss = ["Math", "Filipino", "English", "Computer Science", "Physics"] * 5
+liss = ["Math" * 3, "Filipino" * 3, "English" * 3, "Computer Science" * 5, "Physics" * 5]
 sched.gen_schedule(liss)
 sched.output_schedule()
